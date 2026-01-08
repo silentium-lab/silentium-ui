1a93bd12356fd0df5b9b3761a08f73d35c997868
Eugene Kosukhin <kosuha606@gmail.com>
2026-01-08 22:03:46 +0300

    fix(main): mount component default value

--- src/Mount.ts
+++ src/Mount.ts
@@ -12,11 +12,17 @@ import { Element } from "silentium-web-api";
 import { ClassName } from "./ClassName";
 import { Id } from "./Id";
 
-export function Mount($base: MessageType<string>, tag: string = "div") {
+export function Mount(
+  $base: MessageType<string>,
+  tag: string = "div",
+  defaultValue = "",
+) {
   return Message<string>((resolve, reject) => {
     const dc = DestroyContainer();
     const $id = Shared(Id(Of("mount-point")));
-    Applied($id, (id) => `<${tag} class="${id}"></${tag}>`).then(resolve);
+    Applied($id, (id) => `<${tag} class="${id}">${defaultValue}</${tag}>`).then(
+      resolve,
+    );
     const $el = Element(ClassName($id)).catch(reject);
     const $r = Render($el, $base).catch(reject).then(Void());
     dc.add($el);
